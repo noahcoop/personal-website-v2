0229701ae479d149b232a73a4098dcb50ffa7633
Noah Cooper <noah.cooper@tegus.com>
2024-01-27 20:29:50 -0500

    replace favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "noahcop",
+  title: "noahcoop",
   description: "noah's personal website",
 };
 
