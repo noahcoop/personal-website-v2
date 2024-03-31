04c99880b9a28911ebcfacd646090156fdd5a29e
Noah Cooper <noah.cooper@tegus.com>
2024-03-30 21:27:10 -0400

    update favicon and intro page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>{"noah's personal website"}</div>
-      <div>🚧 under construction 🚧</div>
+      <h3>{"hi, i'm noah 👋"}</h3>
+      <div>{"i'm"} a software developer at <a href="https://www.tegus.com/">tegus</a></div>
+      <div>previously, i was a developer at <a href="https://atomicobject.com/">atomic object</a></div>
+      <div>i studied computer science and math at university of michigan</div>
+      <div>i love hanging out with my cat Leo, making pourover coffee, and enjoying nature</div>
     </main>
   );
 }
